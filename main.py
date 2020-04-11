94ce3aa909f0b752a72cbba8f8c597e2fd131d4a
Gman <mrgman11@yandex.ru>
2020-04-11 20:15:20 +0300

    deleted .html links

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/index.html')
+@app.route('/index')
 def index():
     return render_template('test.html')
 
-@app.route('/my_posts.html')
+@app.route('/my_posts')
 def posts():
     return render_template('posts.html')
 
-@app.route('/post_1.html')
+@app.route('/post_1')
 def post1():
     return render_template('post1.html')
 
-@app.route('/post_2.html')
+@app.route('/post_2')
 def post2():
     return render_template('post2.html')
 
-@app.route('/contacts.html')
+@app.route('/contacts')
 def contacts():
     return render_template('contacts.html')
 
